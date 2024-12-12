6e6a4e0842abcf557170dfe84a8a0b8b346b306a
piman528 <k23086kk@aitech.ac.jp>
2024-12-12 15:18:36 +0900

    男女別の人数を出す機能を追加

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from models import initialize_database
 from routes import blueprints
+from routes.gender import Gender
 
 app = Flask(__name__)
 
@@ -14,7 +15,10 @@ for blueprint in blueprints:
 # ホームページのルート
 @app.route('/')
 def index():
-    return render_template('index.html')
+    gender_data = {
+        "gender_count": [Gender().Count(0), Gender().Count(1)]
+    }
+    return render_template('index.html', gender_data=gender_data)
 
 if __name__ == '__main__':
     app.run(port=8080, debug=True)
