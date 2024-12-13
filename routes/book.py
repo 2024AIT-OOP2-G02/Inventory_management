99d48026c8c48fe51636c97139cb0cedccbb2bda
k23137 <k23137kk@aitech.ac.jp>
2024-12-14 00:09:23 +0900

    貸出データのグラフが表示できるように変更しました
    ・book.pyでのデータ処理をmonth.pyを参考に辞書型に変更しました．
    ・index.htmlのデータ受け取りの部分を辞書型に対応させました．
    
    確認後コメントお願いします

--- routes/book.py
+++ routes/book.py
@@ -1,23 +1,28 @@
-from models import Product
-from models import Order
-from models.db import db
-from peewee import *
+from flask import Blueprint, render_template
+from models import Product, Order
+from peewee import fn
+
+# Blueprintの作成
+book_bp = Blueprint('book', __name__, url_prefix='/book')
 
 def get_book_data():
-    book_name = [] # 本の名前を入れておく配列
-    
-    for product in Product.select():
-        book_name.append(product.name) # 名前の取得
-    
-    book_count = [0] * len(book_name)
-    
-    # 書名ごとにカウントする
-    for i, name in enumerate(book_name):
-        print(name, i)
-        query = Order.select().where(Order.product == (i + 1))
-        for book in query:
-            book_count[i] += 1
-            
-    # print(book_count)
+    # 本の名前と貸出数を集計するクエリ
+    book_data_query = (Product
+        .select(
+            Product.name.alias('name'),
+            fn.COUNT(Order.id).alias('count')
+        )
+        .join(Order, on=(Order.product == Product.id))
+        .group_by(Product.name)
+        .order_by(Product.name)
+    )
 
-    return [book_name,book_count]
+    # 結果を辞書形式に変換
+    book_data = [
+        {
+            'name': product.name,
+            'count': product.count
+        } for product in book_data_query
+    ]
+
+    return book_data
